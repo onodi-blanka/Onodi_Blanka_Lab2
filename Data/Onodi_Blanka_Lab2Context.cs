b1b5a68b29e395ce4d24a500ef0854455de641ea
blanka <blanka@DESKTOP-1UP747C>
2023-10-19 05:40:05 -0900

    Add project files.

---
+++ Data/Onodi_Blanka_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Onodi_Blanka_Lab2.Models;
+
+namespace Onodi_Blanka_Lab2.Data
+{
+    public class Onodi_Blanka_Lab2Context : DbContext
+    {
+        public Onodi_Blanka_Lab2Context (DbContextOptions<Onodi_Blanka_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Onodi_Blanka_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
