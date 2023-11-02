3cf6f8b943d7fee0fb3a5abc44404904bf37e533
blanka <blanka@DESKTOP-1UP747C>
2023-11-02 07:40:01 -0900

    laborator4 full

---
+++ Models/ViewModels/CategoryIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Onodi_Blanka_Lab2.Models.ViewModels
+{
+    public class CategoryIndexData
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<BookCategory> Books { get; set; }
+    }
+}
