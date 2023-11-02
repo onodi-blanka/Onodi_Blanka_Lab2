b99db8a37492b464fe6904ce9ca71bd7f3262a91
blanka <blanka@DESKTOP-1UP747C>
2023-11-02 04:57:14 -0900

    Laborator 4 fara sarcina in plus

---
+++ Migrations/20231102133931_AuthorChanges1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Onodi_Blanka_Lab2.Migrations
+{
+    public partial class AuthorChanges1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
