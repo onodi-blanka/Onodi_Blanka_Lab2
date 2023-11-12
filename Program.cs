63e4a8116f871b19f380c021bb6a1b69a99af926
blanka <blanka@DESKTOP-1UP747C>
2023-11-12 08:59:11 -0900

    lab5 nefinisat

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Onodi_Blanka_Lab2.Data;
+using Microsoft.AspNetCore.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Onodi_Blanka_Lab2Context>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Onodi_Blanka_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Onodi_Blanka_Lab2Context' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Onodi_Blanka_Lab2Context") ?? throw new InvalidOperationException("Connection string 'Muntean_Iris_Lab2Context' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+
+options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryIdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'LibraryIdentityContextConnection' not found.")));
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+ .AddEntityFrameworkStores<LibraryIdentityContext>();
 
 var app = builder.Build();
 
@@ -22,6 +29,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
